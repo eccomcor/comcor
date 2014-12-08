c60d18c69698b3c09360142de1a5a047da68633e
user <user@192.168.2.4>
2014-12-08 17:29:18 -0500

    untested updates to mutate and population generation

--- StockPredictor/src/Mutate.java
+++ StockPredictor/src/Mutate.java
@@ -1,4 +1,46 @@
+import java.util.Random;
+
 
 public class Mutate {
+	private int n;
+	
+	public Mutate(int n){
+		
+		//We might also think about having a more frequent mutation at the end of the run, as crossover becomes less effective
+		this.n = n;
+		
+	}
+	
+public boolean[] mutate(boolean[] individual){
+	
+	Random r = new Random();
+for(int i = 0; i < individual.length; i++){ 
+	if(i < individual.length-1){
+		//mutation rate of 1/n
+	int random = r.nextInt(n);
+	if(random == 1){
+		if(individual[i] == false){
+			individual[i] = true;
+		}
+		else{
+			individual[i] = false;
+		}
+	}//endif2
+	}//endif1
+	else{
+		//rate of 1/(2n) is used to mutate the prediction
+		int temp = r.nextInt(n*2);
+		if(temp == 1){
+			if(individual[i] == false){
+				individual[i] = true;
+			}
+			else{
+				individual[i] = false;
+			}
+		}
+	}
+	}//endfor
+	return individual;
+}
 
 }
--- StockPredictor/src/PopulationGenerator.java
+++ StockPredictor/src/PopulationGenerator.java
@@ -1,4 +1,53 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class PopulationGenerator {
+	//n is number of genes
+	//mu is parent pop size
+	
+	
+	private int n;
+	private int mu;
+
+	
+	public PopulationGenerator(int n, int mu){
+		
+		this.n = n;
+		this.mu = mu;
+		
+		
+		 
+	}
+	
+public boolean[] generateIndividual(){
+	//n+1 denotes the additional bit necessary for the binary "up or down" prediction
+	
+	boolean[] classifier = new boolean[n+1];
+	Random r = new Random();
+	for(int i = 0; i < classifier.length; i++){
+		classifier[i] = r.nextBoolean();
+		}
+	return classifier;
+}
+
+public ArrayList<boolean[]> generatePopulation(){
+	
+	ArrayList<boolean[]> population = new ArrayList<boolean[]>();
+	
+	for (int i = 0; i < mu; i++){
+		population.add(generateIndividual());
+	}
+	return population;
+}
+	
+public int getN(){
+		return n;
+}
+
+public int getMu(){
+	return mu;
+}
+
 
 }
